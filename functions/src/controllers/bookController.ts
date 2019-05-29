88d55c22fcdd095937087e8d332caf0d029dd7ff
Ernesto <ernestoalejandro738@gmail.com>
2019-05-28 22:27:55 -0400

    delete old structure of express

--- functions/src/controllers/bookController.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express';
-
-export class BookController {
-
-  
-  public helloWorld(req: Request, res: Response) {
-    res.send({ message: "hello world"});
-  }
-}
